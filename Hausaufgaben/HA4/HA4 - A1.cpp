717ad67995f3aff4f23b3e7fedda3db5777af3a6
Adrian <adrian.hammermeister55@gmail.com>
2021-11-08 12:36:23 +0100

    HA4

---
+++ Hausaufgaben/HA4/HA4 - A1.cpp
@@ -0,0 +1,40 @@
+// Name: Adrian Hammermeister; Ailina Steg
+// Studiengang: Physik (Bachelor)
+// Matrikelnummer: 221200260; 221200827
+
+#include <iostream>
+
+int A1() {
+	int ziffer = 0;
+
+	while (ziffer < 100) {
+		std::cout << "Bitte geben Sie eine Zahl (ueber 100) ein: ";
+		std::cin >> ziffer;
+		if (ziffer < 100) {
+			std::cout << "Fehler: Bitte eine Zahl ueber 100 eingeben!" << std::endl;
+		}
+	}
+
+	int temp;
+	bool diff = true;
+
+	while (ziffer >= 10 && diff) {
+		temp = ziffer % 10;
+		ziffer /= 10;
+		if ((temp - ziffer % 10) == 0 || (temp - ziffer % 10) == 1) {
+			continue;
+		}
+		else {
+			diff = false;
+		}
+	}
+
+	if (diff) {
+		std::cout << "Differenzpruefung bestanden!" << std::endl;
+	}
+	else {
+		std::cout << "Differenzpruefung nicht bestanden" << std::endl;
+	}
+
+	return 0;
+}
---
+++ Hausaufgaben/HA4/HA4 - A2.cpp
@@ -0,0 +1,25 @@
+//Name: Adrian Hammermeister; Ailina Steg
+// Studiengang: Physik (Bachelor)
+// Matrikelnummer: 221200260; 221200827
+
+#include <iostream>
+
+
+int main() {
+
+	int breite;
+	std::cout << "Bitte geben Sie die Breite der Pyramide an: ";
+	std::cin >> breite;
+
+	for (int i = 0; i < breite; i++) {
+		for (int j = 0; j < breite - i; j++) {
+			std::cout << "  ";
+		}
+		for (int j = 1; j <= (2*i)+1; j++) {
+			std::cout << "* ";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
