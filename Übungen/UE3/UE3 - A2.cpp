70b32918175259d7bd9e1c2804d64231aa97f817
Adrian <adrian.hammermeister55@gmail.com>
2021-12-21 22:28:16 +0100

    Update Übung 3 - A2

--- Übungen/UE3/UE3 - A2.cpp
+++ Übungen/UE3/UE3 - A2.cpp
@@ -5,15 +5,15 @@ int A2() {
 
 	double sumPreisNorm = 0.0;
 	double sumPreisMem = 60.0;
-	short i = 1;
+	short i = 0;
 
 	std::cout << "\t\tPreismodell 1\t\tPreismodell 2" << std::endl;
 	
 	while (sumPreisMem >= sumPreisNorm) {
+		i++;
 		sumPreisNorm += 4.5;
 		sumPreisMem += 2.5;
 		std::cout << i << ".\t\t  " << sumPreisNorm << "\t\t\t    " << sumPreisMem << std::endl;
-		i++;
 	}
 
 
